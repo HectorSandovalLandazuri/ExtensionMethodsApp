14335fc7aa7a501a3ffddddc4850906c9373b537
HectorSandovalLandazuri <88155761+HectorSandovalLandazuri@users.noreply.github.com>
2023-02-09 16:08:45 -0600

    Agregar archivos de proyecto.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,6 @@
+﻿PersonModel person =new PersonModel();
+person.FirstName = "First Name: ".RequestString();
+person.LastName = "Last Name: ".RequestString();
+person.Age="Age: ".RequestInt(0,120);
+person.NumberOfChildren = "Number of Children: ".RequestInt(0,2000);
+
